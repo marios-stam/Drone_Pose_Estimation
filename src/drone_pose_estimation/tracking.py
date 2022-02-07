edfb8d594fd6b6aadb4cb18fa770edf03b8b8662
marios-stam <mariosstamatopoulos1@gmail.com>
2022-02-07 23:31:31 +0100

    worlking ROS  integration

--- src/drone_pose_estimation/tracking.py
+++ src/drone_pose_estimation/tracking.py
@@ -1,7 +1,11 @@
 import numpy as np
 import cv2
 import cv2.aruco as aruco
-from Drone_Pose_Estimation.calibration import calibration
+try:
+    from drone_pose_estimation.calibration import calibration
+except:
+    from calibration import calibration
+
 import glob
 from matplotlib import pyplot as plt
 
@@ -10,33 +14,37 @@ from utilities import getVideoCap
 from graphs import xyz_graph
 from math_operations import rotationMatrixToEulerAngles
 
-MARKER_LENGTH=7.9
+MARKER_LENGTH = 7.9
 
-LIVE_VIDEO_MODE=1
+LIVE_VIDEO_MODE = 1
 
-USB_CAMERA=0
+USB_CAMERA = 0
 
-SIMPLE_POSE_ALGORITHM=True
+SIMPLE_POSE_ALGORITHM = True
 
-XYZ_GRAPH=0
+XYZ_GRAPH = 0
+
 
 def track_live_video(matrix_coefficients, distortion_coefficients):
     cap = getVideoCap(usb=USB_CAMERA)
-    xyz_gr=xyz_graph()
-    
+    xyz_gr = xyz_graph()
+
     while True:
         ret, frame = cap.read()
         # operations on the frame come here
         if SIMPLE_POSE_ALGORITHM:
-            frame,tvec,rpy = pose_estimation_simple(frame, matrix_coefficients, distortion_coefficients)
+            frame, tvec, rpy = pose_estimation_simple(
+                frame, matrix_coefficients, distortion_coefficients)
         else:
-            frame,tvec= pose_estimation(frame, matrix_coefficients, distortion_coefficients)
-        
-        frame=cv2.flip(frame, 1) #flip frame to make it easier for user to test it live
-        
-        if len(tvec)>0:
+            frame, tvec = pose_estimation(
+                frame, matrix_coefficients, distortion_coefficients)
+
+        # flip frame to make it easier for user to test it live
+        frame = cv2.flip(frame, 1)
+
+        if len(tvec) > 0:
             #text="%.1f,%.1f,%.1f"%( tvec[0][0][0],tvec[0][0][1],tvec[0][0][2] )
-            text="%.1f,%.1f,%.1f"%( rpy[0],rpy[1],rpy[2] )
+            text = "%.1f,%.1f,%.1f" % (rpy[0], rpy[1], rpy[2])
             frame = writeTextToFrame(frame, text)
             if XYZ_GRAPH:
                 xyz_gr.update(tvec)
@@ -45,61 +53,70 @@ def track_live_video(matrix_coefficients, distortion_coefficients):
         key = cv2.waitKey(3) & 0xFF
         if key == ord('q'):  # Quit
             break
-                
+
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
 
+
 def writeTextToFrame(frame, text):
-    # font 
-    font = cv2.FONT_HERSHEY_SIMPLEX 
-    # org 
-    org = (50, 50) 
-    # fontScale 
+    # font
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    # org
+    org = (50, 50)
+    # fontScale
     fontScale = 1
-    # Blue color in BGR 
-    color = (0, 0, 255) 
-    # Line thickness of 2 px 
+    # Blue color in BGR
+    color = (0, 0, 255)
+    # Line thickness of 2 px
     thickness = 2
-    # Using cv2.putText() method 
-    frame = cv2.putText(frame, text , org, font,fontScale, color, thickness, cv2.LINE_AA) 
+    # Using cv2.putText() method
+    frame = cv2.putText(frame, text, org, font, fontScale,
+                        color, thickness, cv2.LINE_AA)
     return frame
 
-def track_photos(matrix_coefficients, distortion_coefficients,dirpath="test_photos",image_format="jpg"):   
+
+def track_photos(matrix_coefficients, distortion_coefficients, dirpath="test_photos", image_format="jpg"):
     if dirpath[-1:] == '/':
         dirpath = dirpath[:-1]
 
     images = glob.glob(dirpath+'/' + '*.' + image_format)
-    
-    frames=[]
+
+    frames = []
     for fname in images:
         frame = cv2.imread(fname)
         # operations on the frame come here
         if SIMPLE_POSE_ALGORITHM:
-            frame,tvec = pose_estimation_simple(frame, matrix_coefficients, distortion_coefficients)
+            frame, tvec = pose_estimation_simple(
+                frame, matrix_coefficients, distortion_coefficients)
         else:
-            frame,tvec = pose_estimation(frame, matrix_coefficients, distortion_coefficients)
-        
-        frame=cv2.flip(frame, 1) #flip frame to make it easier for user to test it live
-        
-        if len(tvec)>0:
-            text="%.1f,%.1f,%.1f"%( tvec[0][0][0],tvec[0][0][1],tvec[0][0][2] )
+            frame, tvec = pose_estimation(
+                frame, matrix_coefficients, distortion_coefficients)
+
+        # flip frame to make it easier for user to test it live
+        frame = cv2.flip(frame, 1)
+
+        if len(tvec) > 0:
+            text = "%.1f,%.1f,%.1f" % (
+                tvec[0][0][0], tvec[0][0][1], tvec[0][0][2])
             frame = writeTextToFrame(frame, text)
 
         frames.append(frame)
 
     plt.figure()
-    for i,f in enumerate(frames):
-        plt.subplot(2,3,i+1)
-        plt.imshow( cv2.cvtColor(f, cv2.COLOR_BGR2RGB) )
+    for i, f in enumerate(frames):
+        plt.subplot(2, 3, i+1)
+        plt.imshow(cv2.cvtColor(f, cv2.COLOR_BGR2RGB))
 
     plt.show()
+
 
 def pose_estimation(frame, matrix_coefficients, distortion_coefficients):
     # operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Change grayscale
     # aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)  # Use 5x5 dictionary to find markers
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)  # Use 5x5 dictionary to find markers
+    # Use 5x5 dictionary to find markers
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
     parameters = aruco.DetectorParameters_create()  # Marker detection parameters
     # lists of ids and the corners beloning to each id
 
@@ -108,8 +125,8 @@ def pose_estimation(frame, matrix_coefficients, distortion_coefficients):
                                                             cameraMatrix=matrix_coefficients,
                                                             distCoeff=distortion_coefficients)
 
-    #if no calibration has been done
-    #corners, ids, rejected_img_points = aruco.detectMarkers(gray, aruco_dict,
+    # if no calibration has been done
+    # corners, ids, rejected_img_points = aruco.detectMarkers(gray, aruco_dict,
     #                                                        parameters=parameters)
 
     if np.all(ids is not None):  # If there are markers found by detector
@@ -117,11 +134,14 @@ def pose_estimation(frame, matrix_coefficients, distortion_coefficients):
             # Estimate pose of each marker and return the values rvec and tvec---different from camera coefficients
 
             #rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners[i], 0.02, matrix_coefficients,distortion_coefficients)
-            rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, MARKER_LENGTH, matrix_coefficients, distortion_coefficients)
+            rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(
+                corners, MARKER_LENGTH, matrix_coefficients, distortion_coefficients)
 
-            #(rvec - tvec).any()  # get rid of that nasty numpy value array error
-            aruco.drawDetectedMarkers(frame, corners)  # Draw A square around the markers
-            aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)  # Draw Axis
+            # (rvec - tvec).any()  # get rid of that nasty numpy value array error
+            # Draw A square around the markers
+            aruco.drawDetectedMarkers(frame, corners)
+            aruco.drawAxis(frame, matrix_coefficients,
+                           distortion_coefficients, rvec, tvec, 0.01)  # Draw Axis
             # Display the resulting frame
             # cv2.imshow('frame', frame)
             # Wait 3 milisecoonds for an interaction. Check the key and do the corresponding job.
@@ -132,19 +152,23 @@ def pose_estimation(frame, matrix_coefficients, distortion_coefficients):
             xyz = np.dot(R_T,  T).squeeze()
             rpy = np.deg2rad(cv2.RQDecomp3x3(R_T)[0])
 
-            # font 
-            frame = writeTextToFrame(frame, "%.1f,%.1f,%.1f"%( tvec[0][0][0],tvec[0][0][1],tvec[0][0][2])) 
-            frame = cv2.putText(frame, "%.2f,%.2f,%.2f"%( rpy[0],rpy[1],rpy[2] ) , (50,120), font,fontScale, color, thickness, cv2.LINE_AA) 
+            # font
+            frame = writeTextToFrame(frame, "%.1f,%.1f,%.1f" % (
+                tvec[0][0][0], tvec[0][0][1], tvec[0][0][2]))
+            frame = cv2.putText(frame, "%.2f,%.2f,%.2f" % (
+                rpy[0], rpy[1], rpy[2]), (50, 120), font, fontScale, color, thickness, cv2.LINE_AA)
     else:
         pass
-        #print("mlkia")
+        # print("mlkia")
     return frame
+
 
 def pose_estimation_simple(frame, matrix_coefficients, distortion_coefficients):
     # operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Change grayscale
     # aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)  # Use 5x5 dictionary to find markers
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)  # Use 5x5 dictionary to find markers
+    # Use 5x5 dictionary to find markers
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
     parameters = aruco.DetectorParameters_create()  # Marker detection parameters
     # lists of ids and the corners beloning to each id
 
@@ -152,24 +176,28 @@ def pose_estimation_simple(frame, matrix_coefficients, distortion_coefficients):
                                                             parameters=parameters,
                                                             cameraMatrix=matrix_coefficients,
                                                             distCoeff=distortion_coefficients)
-    
-    tvec,rpy=[],[]
+
+    tvec, rpy = [], []
     if np.all(ids is not None):  # If there are markers found by detector
         for i in range(0, len(ids)):  # Iterate in markers
             # Estimate pose of each marker and return the values rvec and tvec---different from camera coefficients
-            rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners[i], MARKER_LENGTH, matrix_coefficients, distortion_coefficients)
+            rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(
+                corners[i], MARKER_LENGTH, matrix_coefficients, distortion_coefficients)
 
-            #(rvec - tvec).any()  # get rid of that nasty numpy value array error
-            aruco.drawDetectedMarkers(frame, corners)  # Draw A square around the markers
+            # (rvec - tvec).any()  # get rid of that nasty numpy value array error
+            # Draw A square around the markers
+            aruco.drawDetectedMarkers(frame, corners)
 
-            R=cv2.Rodrigues(rvec)[0]
-            rpy=rotationMatrixToEulerAngles(R)
+            R = cv2.Rodrigues(rvec)[0]
+            rpy = rotationMatrixToEulerAngles(R)
 
-    return frame,tvec,rpy
+    return frame, tvec, rpy
 
-if __name__=="__main__":
-    camera_matrix, dist_matrix=calibration.load_coefficients("calibration/cameraCoeffs.yml")
+
+if __name__ == "__main__":
+    camera_matrix, dist_matrix = calibration.load_coefficients(
+        "calibration/cameraCoeffs.yml")
     if(LIVE_VIDEO_MODE):
-        track_live_video(camera_matrix,dist_matrix)
+        track_live_video(camera_matrix, dist_matrix)
     else:
-        track_photos(camera_matrix,dist_matrix)
+        track_photos(camera_matrix, dist_matrix)
